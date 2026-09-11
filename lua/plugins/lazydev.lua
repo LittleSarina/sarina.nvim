96f4549bfe14ad0dc351e8c27dfaf059f0d70ce3
LittleSarina <fallsillyowo@proton.me>
2026-09-11 19:17:51 -0400

    Adeed lazydev, changed some options in obsidian.nvim, added lazy-lock.json to .gitignore

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      libraby = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    ---@module "blink.cmp"
+    ---@type blink.cmp.Config
+    opts = {
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+}
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,20 +1,41 @@
 return {
   {
     "https://github.com/obsidian-nvim/obsidian.nvim",
+    version = "*",
+    ft = "markdown",
+    lazy = false,
+    ---@module 'obsidian'
+    ---@type obsidian.config
     opts = {
-
-      legacy_commands = false, -- this will be removed in 4.0.0
+      legacy_commands = false,
       workspaces = {
         {
-          name = "personal",
-
-          path = "~/vaults/personal",
-        },
-        {
-          name = "work",
-          path = "~/vaults/work",
+          name = "Sarina Palace",
+          path = "~/Documents/obsidian-vaults/Sarina Palace/",
         },
       },
+      frontmatter = { enabled = false },
+      ui = { enabled = false },
+      new_notes_location = "current_dir",
+      picker = {
+        name = "telescope.nvim"
+      }
     },
   },
+  {
+    "folke/snacks.nvim",
+    ---@module 'snacks'
+    ---@type snacks.Config
+    opts = {
+      image = {
+        -- TODO: Find whar SRC means
+        resolve = function (path, src)
+          local api = require("obsidian.api")
+          if api.path_is_note(path) then
+            return api.resolve_attachment_path(src)
+          end
+        end
+      }
+    },
+  }
 }
