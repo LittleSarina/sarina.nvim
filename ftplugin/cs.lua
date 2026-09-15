f546b6dcebe00483ddc74a7a316f8fb7b9339cbb
LittleSarina <fallsillyowo@proton.me>
2026-09-15 17:05:19 -0400

    added a ftplugin folder for c# and lua

---
+++ ftplugin/cs.lua
@@ -0,0 +1,16 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
+
+-- local pairs_present, autopairs = pcall(require, "nvim-autopairs")
+--
+-- if pairs_present then
+--   local Rule = require("nvim-autopairs.rule")
+--
+--   -- Apply the new-line brace behavior rules just for C#
+--   autopairs.add_rules({
+--     Rule("{", "}")
+--       :end_wise()
+--   })
+-- end
